f13ae38b46ba9266ee67f4a980c0c3adecef039e
William A. O. Brown <will.b611@gmail.com>
2017-10-29 22:40:54 +0000

    Enable web management plugin

--- start-rabbitmq.bash
+++ start-rabbitmq.bash
@@ -1,3 +1,6 @@
 #!/bin/bash
-export RABBITMQ_LOGS="/var/log/syslog"
+export HOME="/root"
+## Enable web console
+/usr/sbin/rabbitmq-plugins --offline enable rabbitmq_management
+## Start service
 service rabbitmq-server start
